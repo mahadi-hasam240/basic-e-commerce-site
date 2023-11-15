16047675fc219cca480ff874d4fbbd4a6efdd3ab
mahadi-hasam240 <mahadi.hasan@reddotdigitalit.com>
2023-11-16 01:03:50 +0600

    Discovery service added

---
+++ discovery-service/src/test/java/com/ecommerce,site/discovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,9 @@
+package com.brainstation23.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+}
